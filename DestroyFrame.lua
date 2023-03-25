0a9a2ff5b89614d8fe8dae6d9a5509440d9fc8d4
GalaxyXKedi <117038272+GalaxyXKedi@users.noreply.github.com>
2023-03-25 13:26:31 +0300

    Create DestroyFrame.lua

---
+++ DestroyFrame.lua
@@ -0,0 +1,4 @@
+game.StarterGui.EBonus:Destroy()
+game.Players.LocalPlayer.PlayerGui.BubbleChat:Destroy()
+game.Players.LocalPlayer.PlayerGui.Chat:Destroy()
+game.Players.LocalPlayer.PlayerGui.EBonus:Destroy()
